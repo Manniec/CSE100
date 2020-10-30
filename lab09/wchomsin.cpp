5db19b0da9b08011bb9132fe249d4775ebcb5142
Manniec <manniec00@gmail.com>
2020-10-30 12:59:58 -0700

    read testcase input

---
+++ lab09/wchomsin.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <list>
+#include <string>
+using namespace std;
+
+void printHashTable (list<int>* table, int m){
+    //list <int> :: iterator it; //create list iterator
+    for (int i = 0; i < m; i ++){
+        cout<< i << " : " << *(table[i].begin());
+        for(list <int> :: iterator it = table[i].begin(); it != table[i].end(); it++){
+            cout<<"->"<< *it;
+        }
+        cout<<endl;
+    }
+}
+
+void chainedHashInsert (list<int>* table, int key){
+
+}
+
+bool chainedHashDelete (list<int>* table, int key){
+    return true; //key was found: DELETED
+    return false; //key was not found: DELETE FAILED
+}
+
+int* chainedHashSearch (list<int>* , int key){
+    int* location = new int [2];
+    return location;
+}
+
+int main(){
+    //Read Test Files
+    int m, temp;
+    cin >> m; //size of hash table
+
+    string command = "";
+    while (command != "e"){
+        cin >> command;
+        if (command == "e"){
+            return 0;
+        }else if(command == "o"){
+            cout<<"Output Hashtable"<<endl;
+        }else if (command == "i"){
+            cin >> temp;
+            cout<< "insert "<<temp<<endl;
+        }else if (command == "d"){
+            cin >> temp;
+            cout<< "delete "<<temp<<endl;
+        }else if (command == "s"){
+            cin >> temp;
+            cout << "search "<< temp <<endl;
+        }
+    }
+}
